7d4fdfce618c13489753d8abc21c4783e29bd628
breeeeeeeeze <earth9563@gmail.com>
2022-12-02 18:16:17 +0900

    Update test

--- 19.컬렉션[Collection]/src/generic/Test.java
+++ 19.컬렉션[Collection]/src/generic/Test.java
@@ -3,7 +3,9 @@ package generic;
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("GitHub");
+		System.out.println("Git Staging");
+		System.out.println("Unstaged Changes");
 
 	}
 
