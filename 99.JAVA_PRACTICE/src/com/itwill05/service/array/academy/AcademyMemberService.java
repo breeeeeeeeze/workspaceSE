19a85cf203446b02c340fc403bf08528bd5a662a
breeeeeeeeze <earth9563@gmail.com>
2022-12-05 09:14:27 +0900

    Practice_11-30_Academy

---
+++ 99.JAVA_PRACTICE/src/com/itwill05/service/array/academy/AcademyMemberService.java
@@ -0,0 +1,141 @@
+package com.itwill05.service.array.academy;
+
+/*
+ * AcademyMember들의 업무처리클래스
+ *  Create
+ *  Read
+ *  Update
+ *  Delete
+ */
+public class AcademyMemberService {
+
+	private AcademyMember[] members;
+
+	public AcademyMemberService() {
+		members = new AcademyMember[9];
+		members[0] = new AcademyStudent(1, "KIM", "자바");
+		members[1] = new AcademyStudent(2, "LEE", "리눅스");
+		members[2] = new AcademyStudent(3, "KIM", "자바");
+		members[3] = new AcademyStudent(4, "LEE", "IOT");
+		members[4] = new AcademyGangsa(5, "CHOI", "프로그래밍");
+		members[5] = new AcademyGangsa(6, "KIM", "DB");
+		members[6] = new AcademyGangsa(7, "DIM", "OS");
+		members[7] = new AcademyStaff(8, "AIM", "영업");
+		members[8] = new AcademyStaff(9, "QIM", "생산");
+
+	}
+
+	/*
+	 * 1.AcademyMember 전체출력
+	 */
+	public void print() {
+		for (AcademyMember academyMember : members) {
+			academyMember.print();
+		}
+	}
+
+	/*
+	 * 2-1.번호 1 번 AcademyMember 객체 참조변수반환해줘
+	 */
+	public AcademyMember findByNo(int no) {
+		AcademyMember temp = null;
+		for (int i = 0; i < members.length; i++) {
+			if (members[i].getNo() == no) {
+				temp = members[i];
+				// 확인용 출력
+				System.out.println(temp);
+				break;
+				}
+		}
+		return temp;
+	}
+	
+	/*
+	public AcademyMember member(int no) {
+		AcademyMember findMember = null;
+		for(int i = 0; i < members.length; i++) {
+		if(members[i].getNo() == no) {
+			findMember = members[i];
+			break;
+			}
+		}
+		return findMember;
+	}
+	*/
+
+	/*
+	 * 2-2.이름 KIM 인 AcademyMember 배열객체 참조변수반환해줘
+	 */
+	public AcademyMember findByName(String name) {
+		AcademyMember temp = null;
+		for (int i = 0; i < members.length; i++) {
+			if (members[i].getName().equals(name)) {
+				temp = members[i];
+				// 확인용 출력
+				System.out.println(temp);
+			}
+		}
+		return temp;
+	}
+
+	/*
+	 * 3.AcademyMember 중에서 AcademyStudent객체들 모두반환해줘 [ AcademyMember[] 객체주소반환 ]
+	 * 3.AcademyMember 중에서 AcademyStaff객체들 모두반환해줘 [ AcademyMember[] 객체주소반환 ]
+	 * 3.AcademyMember 중에서 AcademyGangsa객체들 모두반환해줘 [ AcademyMember[] 객체주소반환 ]
+	 */
+	public AcademyMember findStudent() {
+		AcademyMember temp = null;
+		for (int i = 0; i < members.length; i++) {
+			if (members[i] instanceof AcademyStudent) {
+				temp = members[i];
+				System.out.println(temp);
+			}
+		}
+		return temp;
+	}
+	
+	public void findStaff() {
+		for (int i = 0; i < members.length; i++) {
+			if (members[i] instanceof AcademyStaff) {
+				members[i].print();
+			}
+		}
+	}
+	
+	public void findGangsa() {
+		for (int i = 0; i < members.length; i++) {
+			if (members[i] instanceof AcademyGangsa) {
+				members[i].print();
+			}
+		}
+	}
+
+	/*
+	 * 4.AcademyMember 중에서 자바반인   AcademyStudent 객체들 배열참조변수반환해줘 
+	 * 4.AcademyMember 중에서 영업부서인 AcademyStaff   객체들 배열참조변수반환해줘 
+	 * 4.AcademyMember 중에서 자바과목인 AcademyGangsa  객체들 배열참조변수반환해줘
+	 */
+	
+	public void findJavaStudent() {
+		for (int i = 0; i < members.length; i++) {
+			if (members[i] instanceof AcademyStudent && members[i].getClass().equals("자바")) {
+				members[i].print();
+			}
+			
+		}
+	}
+
+	/*
+	 * 5.AcademyMember 중에서 자바반인   AcademyStudent 객체들 배열참조변수반환해줘[OPTION]
+	 * 5.AcademyMember 중에서 영업부서인 AcademyStaff   객체들 배열참조변수반환해줘[OPTION] 
+	 * 5.AcademyMember 중에서 자바과목인 AcademyGangsa  객체들 배열참조변수반환해줘[OPTION]
+	 */
+
+}
+
+
+
+
+
+
+
